1c5ba6628038b9d70c733015f2b4469f17877b8f
flexy4vibez <117163995+flexy4vibez@users.noreply.github.com>
2023-04-23 11:21:14 +0100

    updating images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -21,61 +21,61 @@
     
     {{-- List of Albums --}}
     {{-- Row 1 Recent Uploads --}}
-    <div class="col-sm-12 row justify-content-between text_orange mt-3">
+    <div class="col-sm-12 row justify-content-between mb-4">
 
         <div class="col-sm-3">
-            <img src="images/album_background.png" alt="" class="img-fluid">
-            <img src="images/track1.png" alt="" class="img-fluid track_position">
+            <img src="images/track1.png" alt="" class="img-fluid mt-3">
+            <img src="images/album_background2.png" alt="" class="img-fluid">
+        </div>
+
+        <div class="col-sm-3"> 
+            <img src="images/track2.png" alt="" class="img-fluid mt-3">
+            <img src="images/album_background2.png" alt="" class="img-fluid">
         </div>
 
         <div class="col-sm-3">
-            <img src="images/album_background.png" alt="" class="img-fluid"> 
-            <img src="images/track2.png" alt="" class="img-fluid track_position">
+            <img src="images/track3.png" alt="" class="img-fluid mt-3">
+            <img src="images/album_background2.png" alt="" class="img-fluid">
         </div>
 
         <div class="col-sm-3">
-            <img src="images/album_background.png" alt="" class="img-fluid">
-            <img src="images/track3.png" alt="" class="img-fluid track_position">
-        </div>
-
-        <div class="col-sm-3">
-            <img src="images/album_background.png" alt="" class="img-fluid">
-            <img src="images/track4.png" alt="" class="img-fluid track_position">
+            <img src="images/track4.png" alt="" class="img-fluid mt-3">
+            <img src="images/album_background2.png" alt="" class="img-fluid">
         </div>
 
     </div>
     
 
     {{-- Artists --}}
-    <div class="col-sm-12 row justify-content-between text-white" style="position: relative; top:-16%;">
+    <div class="col-sm-12 row justify-content-between text-white">
         <div class="col-sm-3"><h4 style="font-family: 'Pacifico', cursive;">Artists</h4></div>
         <div class="col-sm-3"><h4>SEE ALL</h4></div>
     </div>
 
     {{-- Row 2 Artists --}}
-    <div class="col-sm-12 row justify-content-between text_orange mt-3" style="position: relative; top:-16%;">
+    <div class="col-sm-12 row justify-content-between text_orange">
 
         <div class="col-sm-3">
-            <img src="images/artist1.png" alt="" class="img-fluid">  
+            <img src="images/artist1.png" alt="" class="img-fluid mt-3">  
         </div>
 
         <div class="col-sm-3">
-            <img src="images/artist2.png" alt="" class="img-fluid"> 
+            <img src="images/artist2.png" alt="" class="img-fluid mt-3"> 
         </div>
 
         <div class="col-sm-3">
-            <img src="images/artist3.png" alt="" class="img-fluid">  
+            <img src="images/artist3.png" alt="" class="img-fluid mt-3">  
         </div>
 
         <div class="col-sm-3">
-            <img src="images/artist4.png" alt="" class="img-fluid">  
+            <img src="images/artist4.png" alt="" class="img-fluid mt-3">  
         </div>
 
     </div>
 
 
     {{-- Footer --}}
-    <div class="container-fluid row justify-content-between footer_background text-white px-o" style="position: relative; top:-16%;">
+    <div class="container-fluid row justify-content-between footer_background text-white px-o">
         <div class="col-sm-4">
             <p class="py-2" style="font-family: 'Pacifico', cursive;">welcome to echoes</p>
             <p class="pb-2">Sign up and get unlimited songs</p>
